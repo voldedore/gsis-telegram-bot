f2330f1182d6b24c5f614834022033af1e0e11c3
Vinh VO <voldedore@gmail.com>
2019-04-17 21:38:45 +0700

    Fix grammar

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"time"
-	"log"
 	"fmt"
-	"strconv"
+	"log"
 	"net/url"
 	"os"
+	"strconv"
+	"time"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
@@ -28,31 +28,31 @@ func gSisBot() {
 	})
 
 	b.Handle(tb.OnText, func(m *tb.Message) {
-		b.Send(m.Sender, "This bot does not currently supported the interactive mode. Please use the inline mode (@GoogleSisBot your_chat)")
+		b.Send(m.Sender, "This bot does not currently support the interactive mode. Please use the inline mode (@GoogleSisBot your_chat)")
 	})
 
 	b.Handle(tb.OnQuery, func(q *tb.Query) {
 		if q.Text != "" {
 			// Declaration of G Translate url (our beloved sister)
 			googleTranslateUrl := "https://translate.google.com.vn/translate_tts?ie=UTF-8&tl=vi&client=tw-ob&q="
-		
+
 			// List of result for inline query
 			results := make(tb.Results, 1)
 
 			// The one we need
 			result := &tb.AudioResult{
 				Title: q.Text,
-				URL: googleTranslateUrl + url.QueryEscape(q.Text),
+				URL:   googleTranslateUrl + url.QueryEscape(q.Text),
 			}
 
 			results[0] = result
 			results[0].SetResultID(strconv.Itoa(0))
-		
+
 			err := b.Answer(q, &tb.QueryResponse{
-				Results: results,
+				Results:   results,
 				CacheTime: 60, // in sec
 			})
-		
+
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -63,7 +63,6 @@ func gSisBot() {
 }
 
 func main() {
-	go gSisBot()	
+	go gSisBot()
 	select {}
 }
-
